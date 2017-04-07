c22924769fad02c9c29d50ee6249ca4948ce28af
arunpatro <arun.kumar.patro@gmail.com>
2017-04-07 20:00:12 +0530

    move trainers to functions

--- cuda/functions.lua
+++ cuda/functions.lua
@@ -10,7 +10,7 @@ function performanceEvaluator(trainset,lenet,eList)
   means = torch.Tensor(length)
   for i=1,length do
     inputs = trainset.images[items[i]]
-    targets = torch.Tensor(1,32,32):fill(trainset.labels[items[i]]*0.3)
+    targets = torch.Tensor(1,1,1):fill(trainset.labels[items[i]]*0.3)
     means[i] = getMean(inputs,targets,lenet)*100/(trainset.labels[items[i]]*0.3)
     print('Image '.. items[i]..' | Label '.. string.format('%2d',trainset.labels[items[i]])  .. ' | Sigma '..string.format('%2.1f', trainset.labels[items[i]]*0.3) ..' | MSE ' .. string.format('%1.8f',criterion:forward(lenet:forward(inputs),targets)) .. ' | Percent Mean ' .. means[i])
   end
@@ -37,18 +37,50 @@ function classPerformance(trainset,lenet,eList,single)
   print(torch.cdiv(acc,hist):mean())
 end
 
-function trainSingle(trainset,lenet,lr,item)
+function trainSingle(trainset,lenet,lr,item,cudaFlag)
   params,grad_params = lenet:getParameters();
-  inputs = trainset.images[item]:cuda()
-  targets = torch.Tensor(1,1,1):fill(trainset.labels[item]*0.3):cuda()
+  if cudaFlag then
+    inputs = trainset.images[item]:cuda()
+    targets = torch.Tensor(1,1,1):fill(trainset.labels[item]*0.3):cuda()
+  else
+    inputs = trainset.images[item]
+    targets = torch.Tensor(1,1,1):fill(trainset.labels[item]*0.3)
+  end
   grad_params:zero();
   outputs = lenet:forward(inputs);
   currentError = currentError + criterion:forward(outputs, targets);
   df_do = criterion:backward(outputs, targets);
   lenet:backward(inputs, df_do);
-  -- params:add(-lr,grad_params);
   -- sgd(params,grad_params,lr)
   -- rmsprop(params,grad_params,lr,0.99,1e-8,config)
   -- adam(params,grad_params,lr,0.9,0.999,1e-7,config)
   adagrad(params,grad_params,lr,1e-7,config)
+end
+
+function trainerBatch(trainset,lenet,lr, bSize, size,cudaFlag)
+  print('Training with batch size ' .. bSize .. ' and learning rate ' .. lr .. ' and size ' .. size)
+  params,grad_params = lenet:getParameters();
+  for t = 1,size,bSize do
+    grad_params:zero();
+    if cudaFlag then
+      inputs = trainset.images[{{t, math.min(t+bSize-1,size)}}]:cuda()
+      targets = torch.Tensor(math.min(t+bSize-1,size)-t+1,1,1,1):cuda()
+      for i=t,math.min(t+bSize-1,size) do
+        targets[i-t+1] = targets[i-t+1]:fill(trainset.labels[i-t+1]*0.3):cuda()
+      end
+    else
+      inputs = trainset.images[{{t, math.min(t+bSize-1,size)}}]
+      targets = torch.Tensor(math.min(t+bSize-1,size)-t+1,1,1,1)
+      for i=t,math.min(t+bSize-1,size) do
+        targets[i-t+1] = targets[i-t+1]:fill(trainset.labels[i-t+1]*0.3)
+      end
+    end
+    outputs = lenet:forward(inputs);
+    f = criterion:forward(outputs, targets);
+    df_do = criterion:backward(outputs, targets);
+    lenet:backward(inputs, df_do);
+    adagrad(params,grad_params,lr,1e-8,config)
+    currentError = currentError + f
+    xlua.progress(t,size)
+  end
 end
--- cuda/minimum.lua
+++ cuda/minimum.lua
@@ -21,28 +21,7 @@ function trainset:size()
   return self.images:size(1)
 end
 
-criterion = nn.MSECriterion()
-criterion = criterion:cuda()
-
-function trainerBatch(lr, bSize, size)
-  print('Training with batch size ' .. bSize .. ' and learning rate ' .. lr .. ' and size ' .. size)
-  params,grad_params = lenet:getParameters();
-  for t = 1,size,bSize do
-    grad_params:zero();
-    inputs = trainset.images[{{t, math.min(t+bSize-1,size)}}]:cuda()
-    targets = torch.Tensor(math.min(t+bSize-1,size)-t+1,1,1,1):cuda()
-    for i=t,math.min(t+bSize-1,size) do
-      targets[i-t+1] = targets[i-t+1]:fill(trainset.labels[i-t+1]*0.3):cuda()
-    end
-    outputs = lenet:forward(inputs);
-    f = criterion:forward(outputs, targets);
-    df_do = criterion:backward(outputs, targets);
-    lenet:backward(inputs, df_do);
-    adagrad(params,grad_params,lr,1e-8,config)
-    currentError = currentError + f
-    xlua.progress(t,size)
-  end
-end
+criterion = nn.MSECriterion():cuda()
 
 config = {
     learningRate = 0.0001,
@@ -68,18 +47,20 @@ logger = optim.Logger('./test.log')
 -- -- Check stats for each class 
 -- performanceEvaluator(trainset,lenet,items)
 
-bSize = 50
+bSize = 100
 size = 1000
-for i=1,500 do
-  currentError = 0
-  trainerBatch(0.001,bSize,size)
-  print('\nPer pixel MSE: ' ..currentError*bSize/size)
-  logger:add{['error'] = currentError*bSize/size}
+for j=1,10 do
+  for i=1,500 do
+    currentError = 0
+    trainerBatch(trainset,lenet,0.001,bSize,size)
+    print('\nEpoch: ' .. (j-1)*500 +i..' Per pixel MSE: ' ..currentError*bSize/size)
+    logger:add{['error'] = currentError*bSize/size}
+  end
+  torch.save('lenet-one.t7',lenet)
 end
-torch.save('lenet-one.t7',lenet)
 
 -- logger:plot()
--- performanceEvaluator(trainset,lenet,torch.range(1,size))
+performanceEvaluator(trainset,lenet)
 classPerformance(trainset,lenet,torch.range(1,size),true)
 
 

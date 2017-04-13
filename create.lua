e65ddc9b57d9950499491960e93f5f939ba21bca
arunpatro <arun.kumar.patro@gmail.com>
2017-04-13 19:31:23 +0530

    simplify creation of datasets

---
+++ create.lua
@@ -0,0 +1,41 @@
+matio = require 'matio'
+
+trname = 'train.t7'
+tename = 'test.t7'
+
+print('Loading .mat file')
+torchDB = matio.load('torchDB.mat')
+imgsTrain = torchDB.trainTorch
+imgsTest = torchDB.testTorch
+
+-- create the trainset and testset in order
+lblsTrain = torch.ceil( torch.range(1,imgsTrain:size()[1]) / (imgsTrain:size()[1]/10) ) 
+lblsTest = torch.ceil(torch.range(1,imgsTest:size()[1]) / (imgsTest:size()[1]/10) ) 
+
+dataset = {
+	images = torch.Tensor(imgsTrain:size()[1],1,32,32):byte(),
+	labels = torch.Tensor(imgsTrain:size()[1]):byte()
+}
+
+dataset2 = {
+	images = torch.Tensor(imgsTest:size()[1],1,32,32):byte(),
+	labels = torch.Tensor(imgsTest:size()[1]):byte()
+}
+
+print('Creating shuffled trainset')
+p = torch.randperm(imgsTrain:size()[1])
+for i = 1,imgsTrain:size()[1] do
+	dataset.images[i] = imgsTrain[p[i]]
+	dataset.labels[i] = lblsTrain[p[i]]
+end
+
+print('Creating shuffled testset')
+p2 = torch.randperm(imgsTest:size()[1])
+for i = 1,imgsTest:size()[1] do
+	dataset2.images[i] = imgsTest[p2[i]]
+	dataset2.labels[i] = lblsTest[p2[i]]
+end
+
+print('Saving datasets')
+torch.save(trname,dataset)
+torch.save(tename,dataset2)
--- create.m
+++ create.m
@@ -7,27 +7,43 @@ for i = 1:80
 end
 images{1} = images{1}(1:640,1:640);
 % create blur kernels
-sigma = 0.05:0.05:3;
+sigma = 0.3:0.3:3;
 kernelSizes = 2*ceil(2*sigma)+1;
-for i = 1:60
+for i = 1:size(sigma,2)
     sigmaFilters{i} = fspecial('gaussian',kernelSizes(i),sigma(i));
 end
 
 % create blurred images
-for i = 1:60
+for i = 1:size(sigma,2)
     for j = 1:80
         blurredImages{(i-1)*80+j} = imfilter(images{j},sigmaFilters{i});
     end
 end
 
-%need to resize 1st image of 640x640 first
-%create blurred patches
-% samples_non_over_32 = samper(blurredImages);
-
 %create individual images by sampling 70/30 of each blurred image
-%(standard)
-standard
+train = {};
+test = {};
+div = 32*ones(1,20);
+n = size(blurredImages,2);
+for ctrn = 1:n
+    sampI = blurredImages{ctrn};
+    disp(ctrn)
+    % for 20x20 = 400 patches per image
+    temp = reshape(mat2cell(sampI,div,div),1,400);
+%     temp = temp(1:4:400);
+    train = [train temp(1:280)];
+    test = [test temp(281:400)];
+end
 
-%save images to path
-seqsaver(test,'test/');
-seqsaver(train,'train/');
+trainTorch = zeros(size(train,2),1,32,32,'uint8');
+testTorch = zeros(size(test,2),1,32,32,'uint8');
+
+%reshaping for torch
+for ctr = 1:size(train,2)
+	trainTorch(ctr,1,:,:) = train{ctr};
+end
+for ctr = 1:size(test,2)
+	testTorch(ctr,1,:,:) = test{ctr};
+end
+
+save('torchDB.mat','trainTorch','testTorch')

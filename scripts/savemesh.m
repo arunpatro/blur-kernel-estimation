cfe6cf35a73a6f7e7a0c026cb649c6c59c29f6ee
arunpatro <arun.kumar.patro@gmail.com>
2017-04-15 23:32:05 +0530

    add matlab visualizer after evaluation

---
+++ scripts/savemesh.m
@@ -0,0 +1,10 @@
+%% savemesh: Saves a 2-D matrix in three views
+function [] = savemesh(name,mat)
+	mesh(mat);
+	saveas(gcf,strcat(name,'_1.jpg'));
+	view(0,0)
+	saveas(gcf,strcat(name,'_2.jpg'));
+	view(0,90)
+	saveas(gcf,strcat(name,'_3.jpg'));
+end
+

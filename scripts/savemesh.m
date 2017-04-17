738ba1ef35d6f3ea8c73f0e5f75c71e8554fc6b6
arunpatro <arun.kumar.patro@gmail.com>
2017-04-17 23:02:00 +0530

    add support for batches

--- scripts/savemesh.m
+++ scripts/savemesh.m
@@ -1,10 +1,13 @@
 %% savemesh: Saves a 2-D matrix in three views
-function [] = savemesh(name,mat)
-	mesh(mat);
+a = dir('*mat');
+for i = 1:size(a,1)
+	load(a(i).name);
+	name = a(i).name(1:end-4);
+	mesh(x);
+    colorbar;
 	saveas(gcf,strcat(name,'_1.jpg'));
-	view(0,t0)
+	view(0,0)
 	saveas(gcf,strcat(name,'_2.jpg'));
 	view(0,-90)
 	saveas(gcf,strcat(name,'_3.jpg'));
 end
-
